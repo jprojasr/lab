255a52a7a79e076d0745fde61ccae4548b3052b3
jprojasr <jp.rojas.roa@gmail.com>
2014-04-09 19:47:53 -0300

    se agrego clase

---
+++ lab02/src/lab02/Ventana.java
@@ -0,0 +1,12 @@
+package lab02;
+
+public class Ventana {
+public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.setTitle("Mi primera ventana");
+		frame.setSize(800, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+
+}
